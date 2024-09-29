4cce4d28cd06233beff7ba31f9e501bee5b6f2da
damiansantos433 <damiannarciso@gmail.com>
2024-09-28 22:37:44 -0700

    Migration

---
+++ src/main/resources/db/migration/V20240908000002__entries.sql
@@ -0,0 +1,4 @@
+ALTER TABLE 'entries'
+    ADD COLUMN `username` VARCHAR(50)           NOT NULL,
+    ADD COLUMN `share_data` VARCHAR(1)          NOT NULL,
+    ADD COLUMN `date_of_birth` VARCHAR(20)      NOT NULL
